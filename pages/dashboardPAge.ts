0904febad26fd9c086fdce81bc88e8ae3a9a4d3b
fsosa9jobs <fsosa9.jobs@gmail.com>
2025-10-06 17:18:17 -0300

    se agrega caso unificado para trx de backend

--- pages/dashboardPAge.ts
+++ pages/dashboardPAge.ts
@@ -6,10 +6,19 @@ export class DashboardPage
   readonly page: Page;
 
   readonly dashboardTitle: Locator;
+  readonly botonAgregarCuenta: Locator;
+  readonly botonEnviarDinero: Locator;
+
+  readonly elementosListaTransferencia: Locator;
+  readonly elementosListaMontoTransferencia: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.dashboardTitle= page.getByTestId('titulo-dashboard');
+    this.botonAgregarCuenta= page.getByTestId('tarjeta-agregar-cuenta');
+    this.botonEnviarDinero= page.getByTestId('boton-enviar');
+    this.elementosListaTransferencia = page.locator('[data-testid="descripcion-transaccion"]');
+    this.elementosListaMontoTransferencia = page.locator('[data-testid="monto-transaccion"]');
 
   }
 
@@ -23,5 +32,10 @@ export class DashboardPage
       console.log('Error al visitar pagina');      
     }
   }
+
+  async visitarPaginaDashboard() {
+        await this.page.goto('http://localhost:3000/dashboard');
+        await this.page.waitForLoadState('networkidle');
+    }
 
 }
---
+++ utils/backendUtils.ts
@@ -0,0 +1,46 @@
+import { expect, APIRequestContext } from "@playwright/test";
+import TestData from '../data/testData.json';
+import { log } from "console";
+
+export class BackendUtils
+{
+  static async crearUsuarioPorAPI(request: APIRequestContext, usuario: any) {
+        const email = (usuario.email.split('@'))[0] + Math.floor(Math.random() * 1000) + '@' + usuario.email.split('@')[1];
+        
+        const datosEnvio = {
+                firstName: usuario.firstName,
+                lastName: usuario.lastName,
+                email: email,
+                password: usuario.password,
+            }
+
+        const response = await request.post('http://localhost:6007/api/auth/signup', {
+            headers: {
+              'Accept': 'application/vnd.github.v3+json',
+                'Content-Type': 'application/json'
+            },
+            data: datosEnvio,
+        })
+        expect(response.status()).toBe(201);
+        return { email: email, password: usuario.password, firstName: usuario.firstName, lastName: usuario.lastName}
+    }
+
+  static async enviarRequestDeBackend(request: APIRequestContext, endpoint: string, email: string)
+  {
+    // Create a repository.
+
+  const response = await request.post(endpoint, {
+    headers: {
+      'Accept': 'application/vnd.github.v3+json',
+      'Content-Type': 'application/json',
+    },
+    data: {
+      firstName: TestData.usuarioValido.firstName,
+      lastName: TestData.usuarioValido.lastName,
+      email: email,
+      password: TestData.usuarioValido.password,
+    },
+  });
+  }
+
+}
